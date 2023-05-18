2381e9c5d7b5f823e315cecee9178aab3b94a7cd
Shub3am <shubhamvishwakarma0604@gmail.com>
2023-05-18 12:58:03 +0530

    Creating Basic Communication

--- public/client-scripts/index.js
+++ public/client-scripts/index.js
@@ -1,16 +1,23 @@
+const messages = document.querySelector("#Messages");
+// const names = document.querySelector("#name");
+const message = document.querySelector("#message-text");
+const send_message = document.querySelector("#message-btn");
+const message_box = document.querySelector(".Message-Box");
+send_message.addEventListener("click", () => {
+  socket.emit("message", message.value);
+});
+
 socket.on("connection", (connection) => {
   console.log("We are Connected", connection);
 });
 
-socket.emit("Hello", "Response from Client");
-socket.on("Hello", (data) => {
-  console.log(data);
-});
-
-socket.on("disconnect", (connection) => {
-  console.log("We are learving", connection);
-});
-
 socket.on("connect", (connection) => {
-  console.log("We are back", connection);
+  socket.emit("New_User");
+});
+socket.on("all_messages", (data) => {
+  data.forEach((single) => {
+    const New_Message = document.createElement("li");
+    New_Message.textContent = single;
+    message_box.appendChild(New_Message);
+  });
 });
--- server.js
+++ server.js
@@ -9,14 +9,27 @@ const io = new Server(httpmodule);
 app.use(express.static(path.join(__dirname + "/public")));
 app.set("views", __dirname);
 app.set("view engine", "ejs");
+const Room = {};
 app.get("/", (req, res) => {
   res.render("client", { welcome: "OK" });
 });
-
+app.get("/c", (req, res) => {
+  res.json(Room);
+});
 io.on("connection", (socket) => {
   socket.emit("Hello", "Welcome to my Server!!");
-  socket.on("Hello", (data) => {
-    console.log(data);
+  socket.on("New_User", () => {
+    Room[socket.id] = { messages: [] };
+  });
+  socket.on("message", (message) => {
+    Room[socket.id].messages.push(message);
+    all_messages = [];
+    for (user of Object.keys(Room)) {
+      Room[user].messages.forEach((single_message) => {
+        all_messages.push(`${user} said ${single_message}`);
+      });
+    }
+    socket.emit("all_messages", all_messages);
   });
 });
 
