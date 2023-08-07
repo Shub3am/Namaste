eb34fe923f44e53e8d9bc1ab26ac8d2343ea93e1
Shub3am <shubhamvishwakarma0604@gmail.com>
2023-08-07 14:52:00 +0530

    Code Refractored

--- client/src/component/ChatBox/ChatBox.tsx
+++ client/src/component/ChatBox/ChatBox.tsx
@@ -1,15 +1,11 @@
 import styles from "./ChatBox.module.css";
+import SideBar from "./Sidebar/SideBar";
 export default function ChatBox() {
   return (
     <div className={styles.main}>
-      <div className={styles.sideBar}>
-        <div className={styles.sideBarchild}>
-          <h1>Group / Person</h1>
-          <p>Recent Message</p>
-          <p>Recent Message Date</p>
-        </div>
+      <div>
+        <SideBar />
       </div>
-      <div></div>
     </div>
   );
 }
