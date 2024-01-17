536387f74e096af569e8cce47f74f2de20f9c418
Shub3am <shubhamvishwakarma0604@gmail.com>
2024-01-18 00:16:38 +0530

    FEAT: Working on First-Time Login Page

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,55 +1,37 @@
 "use client"
 
-import { useEffect, useState } from "react"
-import { io } from "socket.io-client"
-
-function filterSelf(users: any, id:string) {
-  // return users.
-  return users.filter(item=> item!=id)
-}
+import { useState } from "react"
 
 export default function Home() {
-  const socket = io(":8000", {autoConnect: false})
-  const [users, setUsers] = useState([])
-  const [id, setId] = useState("")
-  
+ const [username, setUsername] = useState("")
+ const [isConnected, setConnect] = useState(false)
 
-  useEffect(()=> {
-    
-    socket.connect()
+  if (!isConnected) {
+return (<div className="w-full max-w-md mx-auto p-6"><div className="mt-7 bg-white  rounded-xl shadow-lg dark:bg-gray-800 dark:border-gray-700">
+<div className="p-4 sm:p-7">
+  <div className="text-center">
+    <h1 className="block text-2xl font-bold text-gray-800 dark:text-white">Enter a Username</h1>
 
-    socket.on("connect", ()=> {
-      console.log("Connected!")
-    })
+  </div>
 
-    socket.on("firstConnect", (id)=> {
-      setId(id)
-    })
-
-    socket.on("joined", (newId)=> {
-      console.log([...users,newId], "id", newId)
-      setUsers([...users,newId]);
-      console.log(users)
-    })
-
-    socket.on("left", (userId)=> {
-      let oldUsers = users
-      delete oldUsers[userId]
-      setUsers(oldUsers)
-    })
-    return () => {
-      socket.off("connect")
-      socket.off("joined")
-      socket.off("firstConnect")
-      socket.off("left")
-      socket.disconnect()
-    }
-  }, [])
-  const others = users.map(user=> <li key={user}>{user}</li>)
-  return <div className="p-20 border-white border-solid"><h1>{id}</h1>
-  <h2>Others</h2><ul>{others}</ul>
+  <div className="mt-5">
+    <form onSubmit={(e)=> e.preventDefault()}>
+      <div className="grid gap-y-4">
+        <div>
+          <label htmlFor="name" className="block text-sm font-bold ml-1 mb-2 dark:text-white">So What should we call you?</label>
+          <div className="relative">
+            <input type="text" id="name" onChange={(e)=>setUsername(e.target.value)} name="name" className="py-3 px-4 block w-full border-2 border-gray-200 rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 shadow-sm text-black"/>
+          </div>
+        </div>
+        <button type="submit"  onClick={()=> {
+          (username) ? setConnect(true) : setConnect(false)
+        }} className="py-3 px-4 inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold bg-blue-500 text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-all text-sm dark:focus:ring-offset-gray-800">Connect!</button>
+      </div>
+    </form>
+  </div>
 </div>
-
-
-}
+</div></div>)
+  } else {
+ return <h1>Logged In</h1>
+}}
   
