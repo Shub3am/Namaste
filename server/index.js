37c05a1663d69869ae81b0d6a6382b90a7253a13
Shub3am <shubhamvishwakarma0604@gmail.com>
2024-01-19 00:55:33 +0530

    FEATURE: Implemented Registering and DeRegistering User Function

--- server/index.js
+++ server/index.js
@@ -4,25 +4,27 @@ import { Server } from "socket.io";
 const httpServer = createServer();
 const io = new Server(httpServer, {cors: true});
 
-// io.use(async (socket, next) => {
-//     console.log("Called")
-//     const sockets = await io.fetchSockets()
-//       let connectedUsers = sockets.map(user=>user.id)
-//       io.broadcast("userChange", connectedUsers)
-//   });
-  
+io.use(async (socket, next) => {
+    const sockets = await io.fetchSockets()
+    let connectedUsers = sockets.map(user=>{ return {id: user.id, userName: user.data.userName}})
+    socket.data.users = connectedUsers
+    next()
+  });
+
 
 io.on("connection", (socket) => {
     socket.emit("firstConnect", socket.id)
     socket.on("registerUser", (userName)=> {
-        socket.data.username = userName
+        socket.data.userName = userName
         socket.broadcast.emit("userAdded", {id: socket.id, userName})
+        socket.emit("getUsers", socket.data.users)
+        
     })
 
 
 
 socket.on("disconnect", ()=> {
-    socket.broadcast.emit("userRemoved", {id: socket.id})
+    socket.broadcast.emit("userRemoved", socket.id)
 
     })
 });
