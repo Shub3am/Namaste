81bc55c62850d5131be8fe44d594b7e6c193414c
Shub3am <shubhamvishwakarma0604@gmail.com>
2024-01-18 23:26:53 +0530

    Feature: added listeners for getting Connected Users

--- server/index.js
+++ server/index.js
@@ -3,19 +3,27 @@ import { Server } from "socket.io";
 
 const httpServer = createServer();
 const io = new Server(httpServer, {cors: true});
-let currentRoom = []
-io.on("connection", (socket) => {
-//   console.log(socket.id)
-socket.join("room1")
-currentRoom.push(socket.id)
-socket.to("room1").emit("joined", currentRoom)
-    socket.emit("firstConnect", `Hey ${socket.id}`)
-    socket.broadcast.emit("newUser", socket.id )
 
-    socket.on("disconnect", ()=> {
-        let filterUser = currentRoom.filter(item => item !== socket.id)
-        currentRoom = filterUser
-        socket.broadcast.emit("userLeft", currentRoom)
+// io.use(async (socket, next) => {
+//     console.log("Called")
+//     const sockets = await io.fetchSockets()
+//       let connectedUsers = sockets.map(user=>user.id)
+//       io.broadcast("userChange", connectedUsers)
+//   });
+  
+
+io.on("connection", (socket) => {
+    socket.emit("firstConnect", socket.id)
+    socket.on("registerUser", (userName)=> {
+        socket.data.username = userName
+        socket.broadcast.emit("userAdded", {id: socket.id, userName})
+    })
+
+
+
+socket.on("disconnect", ()=> {
+    socket.broadcast.emit("userRemoved", {id: socket.id})
+
     })
 });
 
